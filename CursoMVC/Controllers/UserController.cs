8a4aedaf91b8eb19d135f9f3b874a6fa2b7e45a2
juanlondonho <juan.londono11@udea.edu.co>
2021-05-26 12:10:41 -0500

    Se agrega base de datos, se listan usuarios y se crean

--- CursoMVC/Controllers/UserController.cs
+++ CursoMVC/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CursoMVC.Models.User;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,39 @@ namespace CursoMVC.Controllers
 {
     public class UserController : Controller
     {
+        private UserDBContext db = new UserDBContext();
+
         // GET: User
         public ActionResult RegisterUser()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult RegisterUser(UserModel model)
+        {
+            try
+            {
+                model.Perfil = "Nivel_1";
+                db.Users.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("ListUsers");
+            }
+            catch
+            {
+                return View();
+            }
+            
+        }
+
+        public ActionResult ListUsers()
+        {
+            var Users = from e in db.Users
+                        orderby e.Cedula
+                        select e;
+            return View(Users);
+        }
+
+
     }
 }
